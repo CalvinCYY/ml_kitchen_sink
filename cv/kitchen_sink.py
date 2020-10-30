792435a48181ba90c8fd1e766b090041f0b381bf
Calvin Yiu <chunnyyiu@hotmail.com>
2020-10-30 15:44:47 +0000

    reorganised files

--- cv/kitchen_sink.py
+++ cv/kitchen_sink.py
@@ -6,7 +6,7 @@ from sklearn import model_selection
 from sklearn.model_selection import GridSearchCV
 from ml_kitchen_sink.cv import params, models
 
-def model_selection_cv(atoms_file, pairs_file, type_of_pred='regression' rs=100, ts=0.2, n_splits=5):
+def model_selection_cv(atoms_file, pairs_file, type_of_pred='regression', param_opt=False, rs=100, ts=0.2, n_splits=5, scoring='neg_mean_absolute_error'):
 
     atoms_df = pd.read_pickle(atoms_file)
     pairs_df = pd.read_pickle(pairs_file)
@@ -24,12 +24,20 @@ def model_selection_cv(atoms_file, pairs_file, type_of_pred='regression' rs=100,
         models = Get_class_models()
         grids = Get_class_params_grid()
 
-    for model in models:
+    for name, model in models:
         for grid in grids:
-            if model.keys() == grid.keys():
-                search = GridSearchCV(model, grid_search, scoring='neg_mean_absolute_error', cv=kfold, verbose=5)
+            if name == grid.keys():
+                search = GridSearchCV(model, grid_search, scoring=scoring, cv=kfold, verbose=5)
                 result = search.fit(X_train, Y_train)
                 print('Best Score: %s' % result.best_score_)
                 print('Best Hyperparameters: %s' % result.best_params_)
             else:
                 continue
+'''
+    for name, model in models:
+        cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+        results.append(cv_results)
+        names.append(name)
+        msg = "%s MAE: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+        print(msg)
+'''
