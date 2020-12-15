07ca49e0e67c6f24a15b1c5f297a74a8a79aee1d
Calvin Yiu <chunnyyiu@hotmail.com>
2020-12-15 15:35:34 +0000

    created function

--- cv/gaussian.py
+++ cv/gaussian.py
@@ -5,92 +5,73 @@ from skopt.utils import use_named_args
 from sklearn.tree import DecisionTreeRegressor
 #from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split, cross_val_score
+#from ml_kitchen_sink.cv import gauss_grid
+#from ml_kitchen_sink.cv import models
 
 import pandas as pd
 import numpy as np
-'''
-To Do:
-    Utilize pipeline fully
-    Tweak surrogate method
-    Implement more models
-    Add in loop for multiple models and params
+
 '''
 def on_step(optim_result):
     """
     View scores after each iteration
     while performing Bayesian
-    Optimization in Skopt"""
+    Optimization in Skopt
+    """
     score = reg_gp.best_score_
     print("best score: %s" % score)
     if score >= -0.90:
         print('Interrupting!')
         return True
+'''
 
-#point to df with x, y data
-test_atoms = "/Users/bd20841/dataset/atoms_df_data4.pkl"
+def gaussian(test_atoms):
 
-atoms_df = pd.read_pickle(test_atoms)
-#pairs_df = pd.read_pickle(pairs_file)
+    atoms_df = pd.read_pickle(test_atoms)
+    #pairs_df = pd.read_pickle(pairs_file)
 
-X = np.array(atoms_df['atomic_rep']).tolist()
-y = atoms_df['shift']
+    X = np.array(atoms_df['atomic_rep']).tolist()
+    y = atoms_df['shift']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
-dtr_space = [
-    Categorical(['mse', 'friedman_mse', 'mae'], name='criterion'),
-    Categorical(['best', 'random'], name='splitter'),
-    Integer(2, 40, name='min_samples_split'),
-    Integer(1, 40, name='min_samples_leaf'),
-    Real(0, 0.5, prior='uniform', name='min_weight_fraction_leaf'),
-    Categorical(['auto', 'sqrt', 'log2'], name='max_features'),
-    Integer(0,10, name='min_impurity_decrease'),
-    #'ccp_alpha': Real(0, 10, prior='log-uniform'),
-]
+    #dtr_space = gauss_grid(regressor)
+    dtr_space = [
+        Categorical(['mse', 'friedman_mse', 'mae'], name='criterion'),
+        Categorical(['best', 'random'], name='splitter'),
+        Integer(2, 40, name='min_samples_split'),
+        Integer(1, 40, name='min_samples_leaf'),
+        Real(0, 0.5, prior='uniform', name='min_weight_fraction_leaf'),
+        Categorical(['auto', 'sqrt', 'log2'], name='max_features'),
+        Integer(0,10, name='min_impurity_decrease'),
+        #'ccp_alpha': Real(0, 10, prior='log-uniform'),
+        ]
 
-regressor = DecisionTreeRegressor()
+    #regressor = models(regressor)
+    regressor = DecisionTreeRegressor()
 
-@use_named_args(dtr_space)
-def objective(**params):
+    @use_named_args(dtr_space)
+    def objective(**params):
         regressor.set_params(**params)
         return -np.mean(cross_val_score(regressor, X_train, y_train, cv=5, scoring='neg_mean_absolute_error'))
 
-reg_gp = gp_minimize(objective, dtr_space, verbose=True)
+    reg_gp = gp_minimize(objective, dtr_space, verbose=True)
 
-print('best score: {}'.format(reg_gp.fun))
+    print('best score: {}'.format(reg_gp.fun))
 
-print('best params:')
-print('               criterion: {}'.format(reg_gp.x[0]))
-print('                splitter: {}'.format(reg_gp.x[1]))
-print('       min_samples_split: {}'.format(reg_gp.x[2]))
-print('        min_samples_leaf: {}'.format(reg_gp.x[3]))
-print('min_weight_fraction_leaf: {}'.format(reg_gp.x[4]))
-print('            max_features: {}'.format(reg_gp.x[5]))
-print('   min_impurity_decrease: {}'.format(reg_gp.x[6]))
+    print('best params:')
+    print('               criterion: {}'.format(reg_gp.x[0]))
+    print('                splitter: {}'.format(reg_gp.x[1]))
+    print('       min_samples_split: {}'.format(reg_gp.x[2]))
+    print('        min_samples_leaf: {}'.format(reg_gp.x[3]))
+    print('min_weight_fraction_leaf: {}'.format(reg_gp.x[4]))
+    print('            max_features: {}'.format(reg_gp.x[5]))
+    print('   min_impurity_decrease: {}'.format(reg_gp.x[6]))
 
-reg_gp.fit(X_test, y_test)
+    reg_gp.fit(X_test, y_test)
 
-dump(reg_gp, 'dtr_result_v2.pkl')
+    dump(reg_gp, 'dtr_result_v2.pkl')
 
-print("val. score: %s" % reg_gp.best_score_)
-print("test score: %s" % reg_gp.score(X_test, y_test))
-print("best params: %s" % str(reg_gp.best_params_))
-
-'''
-def on_step(optim_result):
-    """
-    View scores after each iteration
-    while performing Bayesian
-    Optimization in Skopt"""
-    score = dtr_opt.best_score_
-    print("best score: %s" % score)
-    if score >= -0.90:
-        print('Interrupting!')
-        return True
-
-    dtr_opt.fit(X_train, y_train)
-
-    print("val. score: %s" % dtr_opt.best_score_)
-    print("test score: %s" % dtr_opt.score(X_test, y_test))
-    print("best params: %s" % str(dtr_opt.best_params_))
-'''
+    print("val. score: %s" % reg_gp.best_score_)
+    print("test score: %s" % reg_gp.score(X_test, y_test))
+    print("best params: %s" % str(reg_gp.best_params_))
