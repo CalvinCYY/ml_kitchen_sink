ee90771b49d697f4d8c9c7577aa8510bfdff3b08
Calvin Yiu <chunnyyiu@hotmail.com>
2020-12-31 04:04:44 +0000

    implemented KRR and Random forest

--- cv/gaussian.py
+++ cv/gaussian.py
@@ -3,6 +3,9 @@ from skopt.space import Real, Categorical, Integer
 from skopt.plots import plot_objective, plot_histogram
 from skopt.utils import use_named_args
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import RandomForestRegressor
 #from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split, cross_val_score
 #from ml_kitchen_sink.cv import gauss_grid
@@ -10,6 +13,7 @@ from sklearn.model_selection import train_test_split, cross_val_score
 
 import pandas as pd
 import numpy as np
+from pickle import dump
 
 '''
 def on_step(optim_result):
@@ -25,7 +29,7 @@ def on_step(optim_result):
         return True
 '''
 
-def gaussian(test_atoms):
+def gaussian(test_atoms, alg, file):
 
     atoms_df = pd.read_pickle(test_atoms)
     #pairs_df = pd.read_pickle(pairs_file)
@@ -35,42 +39,103 @@ def gaussian(test_atoms):
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
-    #dtr_space = gauss_grid(regressor)
-    dtr_space = [
-        Categorical(['mse', 'friedman_mse', 'mae'], name='criterion'),
-        Categorical(['best', 'random'], name='splitter'),
-        Integer(2, 40, name='min_samples_split'),
-        Integer(1, 40, name='min_samples_leaf'),
-        Real(0, 0.5, prior='uniform', name='min_weight_fraction_leaf'),
-        Categorical(['auto', 'sqrt', 'log2'], name='max_features'),
-        Integer(0,10, name='min_impurity_decrease'),
-        #'ccp_alpha': Real(0, 10, prior='log-uniform'),
-        ]
+    if alg == 'DTR':
+        space = [
+            Categorical(['mse', 'friedman_mse', 'mae'], name='criterion'),
+            Categorical(['best', 'random'], name='splitter'),
+            Integer(2, 40, name='min_samples_split'),
+            Integer(1, 40, name='min_samples_leaf'),
+            Real(0, 0.5, prior='uniform', name='min_weight_fraction_leaf'),
+            Categorical(['auto', 'sqrt', 'log2'], name='max_features'),
+            Integer(0,10, name='min_impurity_decrease'),
+            #'ccp_alpha': Real(0, 10, prior='log-uniform'),
+            ]
+        regressor = DecisionTreeRegressor()
 
-    #regressor = models(regressor)
-    regressor = DecisionTreeRegressor()
+    elif alg == 'KRR':
+        space = [
+            Real(0.0, 5.0, prior='uniform', name='alpha'),
+            Categorical(['linear', 'laplacian', 'polynomial', 'rbf', 'sigmoid', 'chi2', 'additive_chi2'], name='kernel'),
+            Real(0.0, 5.0, prior='uniform', name='gamma'),
+            Real(0.0, 10.0, prior='uniform', name='degree'),
+            Real(0.0, 5.0, prior='uniform', name='coef0'),
+            ]
+        regressor = KernelRidge()
 
-    @use_named_args(dtr_space)
+    elif alg == 'KNN':
+        space = [
+            Integer(1, 50, name='n_neighbors'),
+            Categorical(['uniform', 'distance'], name='weights'),
+            Categorical(['auto'], name='algorithm'),
+            Integer(10, 100, name='leaf_size'),
+            Integer(1, 10, name='p'),
+            Categorical(['euclidean', 'manhattan', 'chebyshev', 'minkowski'], name='metric'),
+            ]
+        regressor = KNeighborsRegressor()
+
+    elif alg == 'RFR':
+        space = [
+            Integer(100, 1000, name='n_estimators'),
+            Categorical(['mse', 'mae'], name='criterion'),
+            Integer(2, 100, name='min_samples_split'),
+            Integer(1, 100, name='min_samples_leaf'),
+            Real(0, 0.5, prior='uniform', name='min_weight_fraction_leaf'),
+            Categorical(['auto', 'sqrt', 'log2'], name='max_features'),
+            Integer(0,10, name='min_impurity_decrease'),
+            Categorical(['True', 'False'], name='bootstrap'),
+            Categorical(['True', 'False'], name='oob_score'),
+            Real(0.0, 1.0, prior='log-uniform', name='ccp_alpha'),
+            ]
+        regressor = RandomForestRegressor()
+
+    @use_named_args(space)
     def objective(**params):
         regressor.set_params(**params)
         return -np.mean(cross_val_score(regressor, X_train, y_train, cv=5, scoring='neg_mean_absolute_error'))
 
-    reg_gp = gp_minimize(objective, dtr_space, verbose=True)
+    reg_gp = gp_minimize(objective, space, verbose=True)
 
     print('best score: {}'.format(reg_gp.fun))
 
     print('best params:')
-    print('               criterion: {}'.format(reg_gp.x[0]))
-    print('                splitter: {}'.format(reg_gp.x[1]))
-    print('       min_samples_split: {}'.format(reg_gp.x[2]))
-    print('        min_samples_leaf: {}'.format(reg_gp.x[3]))
-    print('min_weight_fraction_leaf: {}'.format(reg_gp.x[4]))
-    print('            max_features: {}'.format(reg_gp.x[5]))
-    print('   min_impurity_decrease: {}'.format(reg_gp.x[6]))
 
-    reg_gp.fit(X_test, y_test)
+    if alg == 'DTR':
+        print('               criterion: {}'.format(reg_gp.x[0]))
+        print('                splitter: {}'.format(reg_gp.x[1]))
+        print('       min_samples_split: {}'.format(reg_gp.x[2]))
+        print('        min_samples_leaf: {}'.format(reg_gp.x[3]))
+        print('min_weight_fraction_leaf: {}'.format(reg_gp.x[4]))
+        print('            max_features: {}'.format(reg_gp.x[5]))
+        print('   min_impurity_decrease: {}'.format(reg_gp.x[6]))
 
-    dump(reg_gp, 'dtr_result_v2.pkl')
+    elif alg == 'KRR':
+        print('                   alpha: {}'.format(reg_gp.x[0]))
+        print('                  kernel: {}'.format(reg_gp.x[1]))
+        print('                   gamma: {}'.format(reg_gp.x[2]))
+        print('                  degree: {}'.format(reg_gp.x[3]))
+        print('                   coef0: {}'.format(reg_gp.x[4]))
+
+    elif alg == 'KNN':
+        print('             n_neighbors: {}'.format(reg_gp.x[0]))
+        print('                 weights: {}'.format(reg_gp.x[1]))
+        print('               algorithm: {}'.format(reg_gp.x[2]))
+        print('               leaf_size: {}'.format(reg_gp.x[3]))
+        print('                       p: {}'.format(reg_gp.x[4]))
+        print('                  metric: {}'.format(reg_gp.x[5]))
+
+    elif alg == 'RFR':
+        print('            n_estimators: {}'.format(reg_gp.x[0]))
+        print('               criterion: {}'.format(reg_gp.x[1]))
+        print('       min_samples_split: {}'.format(reg_gp.x[2]))
+        print('        min_samples_leaf: {}'.format(reg_gp.x[3]))
+        print('min_weight_fraction_leaf: {}'.format(reg_gp.x[4]))
+        print('            max_features: {}'.format(reg_gp.x[5]))
+        print('   min_impurity_decrease: {}'.format(reg_gp.x[6]))
+        print('               bootstrap: {}'.format(reg_gp.x[7]))
+        print('               oob_score: {}'.format(reg_gp.x[8]))
+        print('               ccp_alpha: {}'.format(reg_gp.x[9]))
+
+    dump(reg_gp, file)
 
     print("val. score: %s" % reg_gp.best_score_)
     print("test score: %s" % reg_gp.score(X_test, y_test))
